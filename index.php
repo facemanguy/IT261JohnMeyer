1686acdcacf413464441fa50c4fca825ec6ad7e2
John Meyer <john32meyer@gmail.com>
2021-01-18 20:14:57 -0800

    Base HTML Structure Built

--- index.php
+++ index.php
@@ -1,64 +1,55 @@
 <!doctype html>
 
 <html lang="en">
-<head>
-    <meta charset="utf-8">
 
-    <title>John Meyer Portal</title>
-    <meta name="description" content="John Meyer Portal">
-    <meta name="author" content="John Meyer">
-    <link rel="stylesheet" href="css/portal.css">
-    <link rel="stylesheet" href="css/nav.css">
+<head>
+	<meta charset="utf-8">
+
+	<title>John Meyer Portal</title>
+	<meta name="description" content="John Meyer Portal">
+	<meta name="author" content="John Meyer">
+	<link rel="stylesheet" href="css/portal.css">
+	<link rel="stylesheet" href="css/nav.css">
 </head>
 
 <body>
-     <header>
-     <h1>John Meyer's IT261 Portal</h1>
-     <nav class="topnav" id="myTopnav">
-       <a href="index.html" class="active">Welcome</a>
-       <a href="elements.html">HTML Elements</a>
-       <a href="aia.html">AIA</a>
-       <a href="big/index.html">BIG</a>
-       <a href="http://w3schools.com" target="_blank">W3 Schools</a>
-       <a href="https://www.linkedin.com/learning/" target="_blank">LinkedIn Learning</a>
-       <a href="flowchart.html">Flowchart</a>
-       <a href="fp/index.html">Final Project</a>
-       <a href="contact.php">Contact Sara</a>
-       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-     </nav>
-     </header>
-     
-   <div class="wrapper">
-     
-     <img class="desktop" src="images/desktop.jpg" alt="Photo of an adorable GSD puppy" />
-       
-     <img class="tablet" src="images/tablet.jpg" alt="Photo of an adorable GSD puppy" />
-       
-     <img class="phone" src="images/phone.jpg" alt="Photo of an adorable but large GSD in the lap of his best buddy" />
-       
-       <h2 class="subheader">A little about me!</h2>
-       
-       <p>Replace this Lorem Ipsum with a couple of paragraphs about me. Remember this is a public website so you don't want to put anything on here that you don't want the whole world to see! ;)</p>
-       <p>Replace this Lorem Ipsum with a couple of paragraphs about me. Remember this is a public website so you don't want to put anything on here that you don't want the whole world to see! ;)</p>
-       <p>Replace this Lorem Ipsum with a couple of paragraphs about me. Remember this is a public website so you don't want to put anything on here that you don't want the whole world to see! ;)</p>
-       
-     <footer>
-      <p><small>&copy; 2019 by <a href="contact.php">Sara Newman</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
-    </footer>
-  </div>
-     
-  <!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->
-     
-  <script>
-    function myFunction() {
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-            x.className += " responsive";
-        } else {
-            x.className = "topnav";
-        }
-    }   
- </script>
+	<header>
+		<h1>John Meyer's IT261 Portal</h1>
+		<nav class="topnav" id="Topnav">
+			<a href="index.php" class="active">Home</a>
+			<a href="index.php">Assignments</a>
+			<a href="index.php">Classwork</a>
+			<a href="contact.php">Contact</a>
+			<a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+		</nav>
+	</header>
+
+	<div class="wrapper">
+		<aside>
+			<img class="desktop" src="images/JohnMeyer.jpg" alt="Photo of John Meyer" />
+		</aside>
+		<h2 class="subheader">About me</h2>
+
+		<p>Hello! I'm John Meyer, an aspiring web development student working towards my degree. This is my portal page for my IT 261 Web App programming course at Seattle Central College. In here you'll find several pages I've worked on in PHP as part of my coursework. Enjoy!</p>
+		<p>I always knew I wanted to program since I was little. Of course I wanted to work on games for the longest time. I took a high school game programming course that threw me right into the deep end; My first language was C++! I switched my focus from game development to Web development in 2018 as it seemed the more reasonable option. Around the same time I landed into Seattle University's Web development certificate program. Now I'm looking to capitalize that into a degree at SCC hopefully landing a job in the field shortly after graduating.</p>
+		<p>I have an eclectic set of hobbies from the super nerdy to incredibly sporty. I paint miniatures, play role playing games, ride motorcycles, and am an excellent snowboarder. I spent a season as a snowboard instructor a few years back so in case you ever wanted to learn I can help you out! I can play bass guitar and have been to germany. There's a lot more about me if you ever want to chat.</p>
+
+		<footer>
+			<p><small>&copy; 2021 by <a href="contact.php">John Meyer</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+		</footer>
+	</div>
+
+	<!-- Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon -->
+	<script>
+		function myFunction() {
+			var x = document.getElementById("Topnav");
+			if (x.className === "topnav") {
+				x.className += " responsive";
+			} else {
+				x.className = "topnav";
+			}
+		}
+	</script>
 </body>
 
 </html>
