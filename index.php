45ce1c7359e4a37a14d57ed90c44009110fa0b1e
John Meyer <john32meyer@gmail.com>
2021-01-18 21:23:37 -0800

    Added hw1 and css formating

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 	</header>
 
 	<div class="wrapper">
-		<aside>
+		<aside id="portrait">
 			<img class="desktop" src="images/JohnMeyer.jpg" alt="Photo of John Meyer" />
 		</aside>
 		<h2 class="subheader">About me</h2>
@@ -34,6 +34,15 @@
 		<p>I always knew I wanted to program since I was little. Of course I wanted to work on games for the longest time. I took a high school game programming course that threw me right into the deep end; My first language was C++! I switched my focus from game development to Web development in 2018 as it seemed the more reasonable option. Around the same time I landed into Seattle University's Web development certificate program. Now I'm looking to capitalize that into a degree at SCC hopefully landing a job in the field shortly after graduating.</p>
 		<p>I have an eclectic set of hobbies from the super nerdy to incredibly sporty. I paint miniatures, play role playing games, ride motorcycles, and am an excellent snowboarder. I spent a season as a snowboard instructor a few years back so in case you ever wanted to learn I can help you out! I can play bass guitar and have been to germany. There's a lot more about me if you ever want to chat.</p>
 
+		<div class="centered">
+			<h3>My local MAMP Setup</h2>
+			<img src="images/MAMP.jpg" alt="Local Mamp page">
+		</div>
+		
+		<div class="centered">
+			<h3>The PHP error for HW1</h3>
+			<img src="images/phpError.jpg" alt="My PHP error for HW1">
+		</div>
 		<footer>
 			<p><small>&copy; 2021 by <a href="contact.php">John Meyer</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
 		</footer>
