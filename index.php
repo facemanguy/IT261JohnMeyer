b19e3bd4e0a2f40783d217950ad5322f307d1a5e
John Meyer <john32meyer@gmail.com>
2021-01-25 15:26:28 -0800

    Fixed valadation links / errors

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
 				<img class="image" src="images/phpError.JPG" alt="My PHP error for HW1">
 			</div>		
 			<footer>
-				<p><small>&copy; 2021 by <a href="contact.php">John Meyer</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+				<p><small>&copy; 2021 by <a href="contact.php">John Meyer</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/?uri=referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
 			</footer>
 		</div>
 	</body>
--- website/includes/footer.php
+++ website/includes/footer.php
@@ -1,4 +1,4 @@
 <!-- Footer Content -->
             <footer>
-                <p><small>&copy; 2021 by <a href="contact.php">John Meyer</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+                <p><small>&copy; 2021 by <a href="contact.php">John Meyer</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer?uri=referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
             </footer>
