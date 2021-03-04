cee65c21265edf85d31e40c279f4a089b83a2bac
John Meyer <john32meyer@gmail.com>
2021-03-04 00:57:57 -0800

    Reset content

--- website/config.php
+++ website/config.php
@@ -1,10 +1,5 @@
 <?php
 
-    ob_start();
-    define('DEBUG', 'TRUE');
-
-    include('includes/credentials.php');
-
     define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
 
     date_default_timezone_set('America/Los_Angeles');
@@ -209,16 +204,4 @@
         }
 
     }
-
-    function myError($myFile, $myLine, $errorMsg){
-        if(defined('DEBUG') && DEBUG){
-            echo 'Error in file: <b> '.$myFile.' </b> on line: <b> '.$myLine.' </b>';
-            echo 'Error message: <b> '.$errorMsg.'</b>';
-            die();
-        }else{
-            echo ' Houston, we have a problem!';
-            die();
-        }
-    }
-
 ?>
--- website/gallery.php
+++ website/gallery.php
@@ -1,41 +1,6 @@
 <?php
     include('config.php');
     include('includes/header.php');
-
-	// if(isset($_GET['id'])){
-	// 	$id = (int)$_GET['id'];
-	// }else{
-	// 	header('Location:index.php');
-	// }
-	
-	$sql = 'SELECT * FROM FantasyFolk';
-	
-	$iConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));
-	
-	$result = mysqli_query($iConn, $sql) or die(myError(__FILE__,__LINE__,mysqli_error($iConn)));
-	
-	if(mysqli_num_rows($result) > 0){
-		while($row = mysqli_fetch_assoc($result)){
-			echo '<ul>';
-			echo '<img src="images/folk'.$id.'.jpg" alt="'.$Name.'">';
-			echo '<li><b>Name:</b> '.$Name.'</li>';
-			echo '<li><b>Homeland:</b> '.$Homeland.'</li>';
-			echo '<li><b>Rarity:</b> '.$Rarity.'</li>';
-			echo '</ul>';
-			echo '<p>'.$Description.'</p>';
-			
-			// $Name = stripslashes($row['Name']);
-			// $Homeland = stripslashes($row['Homeland']);
-			// $Rarity = stripslashes($row['Rarity']);
-			// $Description = stripslashes($row['Description']);
-			// $feedback = '';
-		}
-	}else{
-		echo'Nobody is home';
-	}
-
-	mysqli_free_result($result);
-	mysqli_close($iConn);
 ?>
 
 <!-- BEGIN GALLERY CONTENT -->
@@ -43,9 +8,19 @@
             <header>
                 <h1>John Meyer's IT261 Gallery Page</h1>
             </header>
-			
-            <?php include('includes/footer.php');?>
+			<div class="showcase">
+				<h3>IMAGE BOX</h3>
+			</div>
+			<div class="content">
+				<h3>CONTENT 1</h3>
+			</div>
+			<div class="content">
+				<h3>CONTENT 2</h3>
+			</div>
 
+			
+            <?php include('includes/footer.php')?>
+			
 		</div>
 	</body>
 </html>
