e8eb33783da3e0664f3b500416d9bbe537873fef
John Meyer <john32meyer@gmail.com>
2021-03-04 01:21:02 -0800

    Gallery html done w8 hw complete

--- website/gallery.php
+++ website/gallery.php
@@ -4,15 +4,40 @@
 ?>
 
 <!-- BEGIN GALLERY CONTENT -->
-		<div class="gridContainer">
+		<div class="galleryBox">
             <header>
                 <h1>John Meyer's IT261 Gallery Page</h1>
             </header>
-			<div class="galleryBox">
+			<div>
 				<table>
 					<tr>
-						<td><img src="images/folk1.jpg" alt="Orcs"></td>
+						<td><img src="images/folk1.jpg" alt="Orcs" class="image"></td>
 						<td>Orcs</td>
+						<td>Common</td>
+					</tr>
+					<tr>
+						<td><img src="images/folk2.jpg" alt="Elves" class="image"></td>
+						<td>Elves</td>
+						<td>Uncommon</td>
+					</tr>
+					<tr>
+						<td><img src="images/folk3.jpg" alt="Dwarves" class="image"></td>
+						<td>Dwarves</td>
+						<td>Uncommon</td>
+					</tr>
+					<tr>
+						<td><img src="images/folk4.jpg" alt="Merfolk" class="image"></td>
+						<td>Merfolk</td>
+						<td>Rare</td>
+					</tr>
+					<tr>
+						<td><img src="images/folk5.jpg" alt="Avian" class="image"></td>
+						<td>Avians</td>
+						<td>Rare</td>
+					</tr>
+					<tr>
+						<td><img src="images/folk6.jpg" alt="Iron Lords" class="image"></td>
+						<td>Iron Lords</td>
 						<td>Common</td>
 					</tr>
 				</table>
