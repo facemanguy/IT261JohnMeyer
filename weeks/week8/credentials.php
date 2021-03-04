b45a2d6bddc77ed41a65ce7f71375541bb0e19de
John Meyer <john32meyer@gmail.com>
2021-03-03 21:07:49 -0800

    credential change

--- weeks/week8/credentials.php
+++ weeks/week8/credentials.php
@@ -2,7 +2,7 @@
 // credentials for week 8
 
 define('DB_NAME', 'it261db');
-define('DB_HOST', 'localhost');
+define('DB_HOST', '144.126.217.66');
 define('DB_USER', 'johnm');
 define('DB_PASSWORD', 'hpdolphin32');
 
