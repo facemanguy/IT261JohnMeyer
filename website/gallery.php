e55ab5f5402be4850226e1e3f70267a44dd706cf
John Meyer <john32meyer@gmail.com>
2021-03-04 00:41:06 -0800

    Conformed to people instead of people-view

--- website/gallery.php
+++ website/gallery.php
@@ -3,11 +3,11 @@
     include('includes/header.php');
 	include('includes/credentials.php');
 
-	if(isset($_GET['id'])){
-		$id = (int)$_GET['id'];
-	}else{
-		header('Location:index.php');
-	}
+	// if(isset($_GET['id'])){
+	// 	$id = (int)$_GET['id'];
+	// }else{
+	// 	header('Location:index.php');
+	// }
 	
 	$sql = 'SELECT * FROM FantasyFolk WHERE FantasyFolkID = '.$id.'';
 	
@@ -17,14 +17,22 @@
 	
 	if(mysqli_num_rows($result) > 0){
 		while($row = mysqli_fetch_assoc($result)){
-			$Name = stripslashes($row['Name']);
-			$Homeland = stripslashes($row['Homeland']);
-			$Rarity = stripslashes($row['Rarity']);
-			$Description = stripslashes($row['Description']);
-			$feedback = '';
+			echo '<ul>';
+			echo '<img src="images/folk'.$id.'.jpg" alt="'.$Name.'">';
+			echo '<li><b>Name:</b> '.$Name.'</li>';
+			echo '<li><b>Homeland:</b> '.$Homeland.'</li>';
+			echo '<li><b>Rarity:</b> '.$Rarity.'</li>';
+			echo '</ul>';
+			echo '<p>'.$Description.'</p>';
+			
+			// $Name = stripslashes($row['Name']);
+			// $Homeland = stripslashes($row['Homeland']);
+			// $Rarity = stripslashes($row['Rarity']);
+			// $Description = stripslashes($row['Description']);
+			// $feedback = '';
 		}
 	}else{
-		$feedback = 'Nobody is home';
+		echo'Nobody is home';
 	}
 ?>
 
@@ -33,26 +41,7 @@
             <header>
                 <h1>John Meyer's IT261 Gallery Page</h1>
             </header>
-			<h1>Yay!</h1>
-			<h2>Currently viewing <?php echo $FirstName;?>'s page</h2>
-			<?php
-				if($feedback == ''){
-					echo '<ul>';
-					echo '<li><b>Name:</b> '.$Name.'</li>';
-					echo '<li><b>Homeland:</b> '.$Homeland.'</li>';
-					echo '<li><b>Rarity:</b> '.$Rarity.'</li>';
-					echo '</ul>';
-					echo '<p>'.$Description.'</p>';
-					echo '<p><a href="gallery.php">Return to gallery page</a></p>';
-				}else{
-					echo $feedback;
-				}
-			?>
-			<?php
-				if($feedback == ''){
-					echo '<img src="images/folk'.$id.'.jpg" alt="'.$Name.'">';
-				}
-			?>
+			
             <?php include('includes/footer.php')?>
 			<?php
 				mysqli_free_result($result);
