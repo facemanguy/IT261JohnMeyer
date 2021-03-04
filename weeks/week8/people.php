d15ba71591df21a39cd26c9a6241954392f5a147
John Meyer <john32meyer@gmail.com>
2021-03-03 21:11:29 -0800

    fixed error on table call

--- weeks/week8/people.php
+++ weeks/week8/people.php
@@ -2,7 +2,7 @@
 
 include('config.php');
 
-$sql = 'SELECT * FROM it261db';
+$sql = 'SELECT * FROM Week8People';
 $iConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));
 
 $result = mysqli_query($iConn, $sql) or die(myError(__FILE__,__LINE__,mysqli_error($iConn)));
