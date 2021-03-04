e8846d41b90c32635e9ef1cd49e0ba335c04016c
John Meyer <john32meyer@gmail.com>
2021-03-04 00:12:58 -0800

    credientals copieed

--- website/config.php
+++ website/config.php
@@ -3,7 +3,7 @@
     ob_start();
     define('DEBUG', 'TRUE');
 
-    include('/weeks/week8/credentials.php');
+    include('includes/credentials.php');
 
     define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
 
