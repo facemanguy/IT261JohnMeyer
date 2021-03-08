4e944aacff9420631ca85c053f3f31fd66a82026
John Meyer <john32meyer@gmail.com>
2021-03-08 01:56:15 -0800

    added login logic to server php

--- weeks/week9/server.php
+++ weeks/week9/server.php
@@ -76,4 +76,31 @@ if(isset($_POST['reg_user'])){
     }
 }
 
+if(isset($_POST['login_user'])){
+    $UserName = mysqli_real_escape_string($db, $_POST['UserName']);
+    $Password = mysqli_real_escape_string($db, $_POST['Password']);
+    
+    if(empty($UserName)){
+        array_push($errors, 'Username is required');
+    }
+
+    if(empty($Password)){
+        array_push($errors, 'Password is required');
+    }
+
+    if(count($errors) == 0){
+        $Password = md5($Password);
+        $query = "SELECT * FROM Users WHERE UserName = '$UserName' AND Password = '$Password'";
+        $results = mysqli_query($db, $query);
+
+        if(mysqli_num_rows($results) == 1){
+            $_SESSION['UserName'] = $UserName;
+            $_SESSION['success'] =$success;
+            header('Location:index.php');
+        }else{
+            array_push($errors, '<p class="red">Wrong Username or password</p>');
+        }
+    }
+}
+
 ?>
