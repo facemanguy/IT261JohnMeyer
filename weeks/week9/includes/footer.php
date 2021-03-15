a2e87800ed569e8f7aba948d1f4e7dad6853c452
John Meyer <john32meyer@gmail.com>
2021-03-15 05:38:22 -0700

    added some styles and validation links

--- weeks/week9/includes/footer.php
+++ weeks/week9/includes/footer.php
@@ -1,5 +1,5 @@
         <footer>
-            <p class="center">Footer section</p>
+            <p class="center"><small>&copy; 2021 by <a href="contact.php">John Meyer</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/?uri=referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
         </footer>
     </body>
 </html>
