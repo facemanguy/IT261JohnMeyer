979a42da77c4207e242bb845989b11ef500c6ec3
John Meyer <john32meyer@gmail.com>
2021-03-25 20:18:59 -0700

    added about

--- Teriyaki/about.php
+++ Teriyaki/about.php
@@ -6,11 +6,11 @@
 <!-- BEGIN ABOUT CONTENT -->
 		<div class="gridContainer">
             <header>
-                <h1>John Meyer's IT261 About Page</h1>
+                <h1>About Tim's</h1>
             </header>
-			<div class="showcase2">
-				<h3>My SQL Data</h3>
-				<img class="image" src="images/sqlData.JPG" alt="Adminer sql data on crabs page">
+			<div class="showcase">
+				<h3>Tims secure SQL Data</h3>
+				<img class="image" src="images/database.jpg" alt="Teriyaki data base">
 			</div>
 		
             <?php include('includes/footer.php')?>
