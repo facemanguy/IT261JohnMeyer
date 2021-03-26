2cef442c8fe879810407f013bd45acd372f73c6c
John Meyer <john32meyer@gmail.com>
2021-03-25 22:10:30 -0700

    test index

--- Teriyaki/index.php
+++ Teriyaki/index.php
@@ -1,41 +1,7 @@
 <?php
-
-session_start();
-include('config.php');
-
-if(!isset($_SESSION['UserName'])){
-    $_SESSION['msg'] = 'You must login';
-    header('Location:login.php');
-}
-
-if(isset($_GET['logout'])){
-    session_destroy();
-    unset($_SESSION['UserName']);
-    header('Location:login.php');
-}
-
-include('includes/header.php');
+    include('config.php');
+    include('includes/header.php');
 ?>
-
-<?php
-// Notification
-if(isset($_SESSION['success'])):?>
-    <div class="success">
-        <h3>
-            <?php
-                echo $_SESSION['success'];
-                unset($_SESSION['success']);
-            ?>
-        </h3>
-    </div>
-<?php endif;?>
-
-<?php if(isset($_SESSION['UserName'])):?>
-    <div class="welcome-logout">
-        <h3>Hello, <?php echo $_SESSION['UserName'];?></h3>
-        <a href="index.php?logout='1'">Log Out</a>
-    </div>
-<?php endif;?>
 
 <!-- BEGIN MAIN CONTENT -->
 		<div class="gridContainer">
